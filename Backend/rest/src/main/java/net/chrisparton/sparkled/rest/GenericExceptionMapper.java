044f6b237589f52ffba7ff6210bc9f67d8b217e4
Chris Parton <chrisparton1991@gmail.com>
2017-10-09 23:35:43 +1100

    Improve exception mapper to pass through 404s instead of masking them as 500 errors.

--- Backend/rest/src/main/java/net/chrisparton/sparkled/rest/GenericExceptionMapper.java
+++ Backend/rest/src/main/java/net/chrisparton/sparkled/rest/GenericExceptionMapper.java
@@ -1,5 +1,6 @@
 package net.chrisparton.sparkled.rest;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,13 +10,29 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
-    private UriInfo uriInfo;
+
+    private final UriInfo uriInfo;
 
     public GenericExceptionMapper(@Context UriInfo uriInfo) {
         this.uriInfo = uriInfo;
     }
+
     @Override
     public Response toResponse(Throwable t) {
+        if (t instanceof NotFoundException) {
+            return returnNotFound();
+        } else {
+            return returnInternalError();
+        }
+    }
+
+    private Response returnNotFound() {
+        return Response.status(Response.Status.NOT_FOUND)
+                .location(uriInfo.getRequestUri())
+                .build();
+    }
+
+    private Response returnInternalError() {
         return Response
                 .status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity("An unexpected error occurred.")
