816531918fea568cd3424c7167f3835c24ff27b3
Chris Parton <chrisparton1991@gmail.com>
2017-10-11 21:18:07 +1100

    Integrate redux with redux-promise middleware

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 import HomePage from './pages/Home/HomePage';
 import EditSongPage from './pages/EditSong/EditSongPage';
 import SchedulerPage from './pages/Scheduler/SchedulerPage';
 import SongListPage from './pages/SongList/SongListPage';
 
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/scheduler' component={SchedulerPage}/>
-      <Route exact path='/songs' component={SongListPage}/>
-      <Route exact path='/songs/:songId' component={EditSongPage}/>
-    </Switch>
-  </Router>
+  <Provider store={createStoreWithMiddleware(() => {})}>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/scheduler' component={SchedulerPage}/>
+        <Route exact path='/songs' component={SongListPage}/>
+        <Route exact path='/songs/:songId' component={EditSongPage}/>
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
