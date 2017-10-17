ff0adf3e30987b237e6c6154a0900ada7961c52a
Chris Parton <chrisparton1991@gmail.com>
2017-10-17 21:43:14 +1100

    Make SongListPage the index page. Add redirect to index for invalid URLs.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from '../../pages/Home';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import EditSongPage from '../../pages/EditSong';
 import SchedulerPage from '../../pages/Scheduler';
 import SongListPage from '../../pages/SongList';
 import store from '../../store/store';
 import './bootswatch-superhero-v4-alpha.css';
 
+const RedirectInvalidUrlToIndex = () => <Redirect to='/'/>;
+
 const App = () => (
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/scheduler' component={SchedulerPage}/>
-        <Route exact path='/songs' component={SongListPage}/>
+        <Route exact path='/' component={SongListPage}/>
         <Route exact path='/songs/:songId' component={EditSongPage}/>
+        <Route exact path='/scheduler' component={SchedulerPage}/>
+        <Route component={RedirectInvalidUrlToIndex}/>
       </Switch>
     </Router>
   </Provider>
