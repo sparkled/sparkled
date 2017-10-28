386a92bfa4b29c6ea4ed3230835ca8b2dd7570ed
Chris Parton <chrisparton1991@gmail.com>
2017-10-28 14:43:39 +1100

    Fix Json date parsing timezone issue.

--- Backend/rest/src/main/java/net/chrisparton/sparkled/rest/json/GsonProvider.java
+++ Backend/rest/src/main/java/net/chrisparton/sparkled/rest/json/GsonProvider.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class GsonProvider {
-    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
     private static final Gson gson = new GsonBuilder()
             .setDateFormat(DATE_FORMAT)
             .registerTypeHierarchyAdapter(byte[].class, new UnsignedByteArrayTypeAdapter())
