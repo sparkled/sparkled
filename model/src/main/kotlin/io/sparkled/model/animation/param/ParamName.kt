c077179cff697864a1f8c3e381f8e42eb467a475
Chris Parton <chrisparton1991@gmail.com>
2018-11-24 23:19:43 +1100

    Add brightness parameter to rainbow fill (#102). Use emptyList() (a global constant) instead of arrayList() as default Param value to save memory. Add default values to parameters.

--- model/src/main/kotlin/io/sparkled/model/animation/param/ParamName.kt
+++ model/src/main/kotlin/io/sparkled/model/animation/param/ParamName.kt
@@ -1,6 +1,7 @@
 package io.sparkled.model.animation.param
 
 enum class ParamName constructor(val displayName: String) {
+    BRIGHTNESS("Brightness"),
     COLOR("Color"),
     CYCLE_COUNT("Cycle Count"),
     CYCLES_PER_SECOND("Cycles Per Second"),
--- renderer/src/main/kotlin/io/sparkled/renderer/fill/function/RainbowFill.kt
+++ renderer/src/main/kotlin/io/sparkled/renderer/fill/function/RainbowFill.kt
@@ -10,15 +10,17 @@ import java.awt.Color
 class RainbowFill : FillFunction {
 
     override fun fill(ctx: RenderContext, led: Led, alpha: Float) {
-        val cycleCount = ParamUtils.getDecimalValue(ctx.effect.getFill()!!, ParamName.CYCLE_COUNT)
-        val cyclesPerSecond = ParamUtils.getDecimalValue(ctx.effect.getFill()!!, ParamName.CYCLES_PER_SECOND)
+        val fill = ctx.effect.getFill()!!
+        val cycleCount = ParamUtils.getDecimalValue(fill, ParamName.CYCLE_COUNT, 1f)
+        val cyclesPerSecond = ParamUtils.getDecimalValue(fill, ParamName.CYCLES_PER_SECOND, 1f)
+        val brightness = ParamUtils.getDecimalValue(fill, ParamName.BRIGHTNESS, 100f) / 100f
 
         val frame = ctx.frame
         val ledPosition = led.ledNumber.toFloat() / frame.ledCount * cycleCount
         val progress = frame.frameNumber.toFloat() / ctx.sequence.getFramesPerSecond()!! * cyclesPerSecond
         val hue = ledPosition + progress % 1f
 
-        val color = Color.getHSBColor(hue, SATURATION, alpha)
+        val color = Color.getHSBColor(hue, SATURATION, alpha * brightness)
         led.addColor(color)
     }
 
