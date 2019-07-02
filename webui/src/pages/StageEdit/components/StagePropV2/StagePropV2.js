f6a704fcebd52b16c9769e2eabd7f00ac6c436d9
Chris Parton <chrisparton1991@gmail.com>
2019-07-02 22:52:00 +1000

    Implement rotation in stage editor

--- webui/src/pages/StageEdit/components/StagePropV2/StagePropV2.js
+++ webui/src/pages/StageEdit/components/StagePropV2/StagePropV2.js
@@ -9,7 +9,10 @@ import stagePropTypes from '../../stagePropTypes';
 const lineWidth = 2;
 const lineColor = 0xffffff;
 const padding = 5;
-const names = { background: "Background", path: "Path" };
+const handleRadius = 5;
+const quarterTurn = Math.PI / 2;
+const fullCircle = Math.PI * 2;
+const names = { background: "Background", path: "Path", rotateHandle: "RotateHandle" };
 
 class StagePropV2 extends Component {
 
@@ -40,12 +43,17 @@ class StagePropV2 extends Component {
   buildStageProp() {
     const { pixiApp, stageProp } = this.props;
 
-    const pathProperties = svgPathProperties(stagePropTypes[stageProp.type].path);
-    const { points, width, height } = this.getLinePoints(pathProperties, stageProp);
+    const { path, segments } = stagePropTypes[stageProp.type];
+    const pathProperties = svgPathProperties(path);
+    const points = this.getLinePoints(pathProperties, stageProp, segments);
 
-    const pixiContainer = this.buildContainer(stageProp, width, height);
-    pixiContainer.addChild(this.buildBackground(stageProp, width, height));
-    pixiContainer.addChild(this.buildPath(points));
+    const pixiPath = this.buildPath(points);
+    const pathWidth = pixiPath.width - lineWidth;
+    const pathHeight = pixiPath.height - lineWidth;
+    const pixiContainer = this.buildContainer(stageProp, pathWidth, pathHeight);
+    pixiContainer.addChild(this.buildBackground(stageProp, pathWidth, pathHeight));
+    pixiContainer.addChild(pixiPath);
+    pixiContainer.addChild(this.buildRotateHandle(stageProp, pathWidth, pathHeight));
     pixiApp.stage.addChild(pixiContainer);
 
     this.setState({ pixiContainer });
@@ -70,48 +78,13 @@ class StagePropV2 extends Component {
 
     background.buttonMode = true;
     background.interactive = true;
-    background.on('pointerup', () => this.props.selectStageProp(stageProp.uuid));
-
     background
-      .on('pointerdown', this.onDragStart)
+      .on('pointerdown', event => this.onDragStart(event, names.background))
       .on('pointermove', this.onDragMove)
       .on('pointerup', this.onDragEnd)
       .on('pointerupoutside', this.onDragEnd);
 
     return background;
-  }
-
-  onDragStart = event => {
-    const { pixiContainer } = this.state;
-    this.setState({
-      dragState: {
-        x: pixiContainer.x,
-        y: pixiContainer.y,
-        mouseX: event.data.originalEvent.clientX,
-        mouseY: event.data.originalEvent.clientY
-      }
-    });
-  }
-
-  onDragMove = event => {
-    const { dragState, pixiContainer } = this.state;
-
-    if (dragState) {
-      pixiContainer.x = dragState.x + (event.data.originalEvent.clientX - dragState.mouseX);
-      pixiContainer.y = dragState.y + (event.data.originalEvent.clientY - dragState.mouseY);
-    }
-  }
-
-  onDragEnd = event => {
-    const { stageProp } = this.props;
-    const { dragState } = this.state;
-
-    this.setState({ dragState: null });
-    this.props.updateStageProp({
-      ...stageProp,
-      positionX: stageProp.positionX + (event.data.originalEvent.clientX - dragState.mouseX),
-      positionY: stageProp.positionY + (event.data.originalEvent.clientY - dragState.mouseY)
-    });
   }
 
   buildPath(points) {
@@ -125,33 +98,94 @@ class StagePropV2 extends Component {
     return path;
   }
 
-  getLinePoints(pathProperties, stageProp) {
+  buildRotateHandle(stageProp, width, height) {
+    const rotateHandle = new PIXI.Graphics();
+    rotateHandle.name = names.rotateHandle;
+
+    rotateHandle.beginFill(0x00FF00);
+    rotateHandle.drawCircle(0, 0, handleRadius);
+    rotateHandle.endFill();
+    rotateHandle.position = { x: width / 2, y: handleRadius * -4 };
+
+    rotateHandle.buttonMode = true;
+    rotateHandle.interactive = true;
+    rotateHandle
+      .on('pointerdown', event => this.onDragStart(event, names.rotateHandle))
+      .on('pointermove', this.onDragMove)
+      .on('pointerup', this.onDragEnd)
+      .on('pointerupoutside', this.onDragEnd);
+
+    return rotateHandle;
+  }
+
+  onDragStart = (event, target) => {
+    const { pixiContainer } = this.state;
+    this.props.selectStageProp(this.props.stageProp.uuid);
+
+    const { x, y } = event.data.getLocalPosition(pixiContainer.parent);
+    this.setState({
+      dragState: { target, originX: pixiContainer.x, originY: pixiContainer.y, mouseX: x, mouseY: y }
+    });
+  }
+
+  onDragMove = event => {
+    const { dragState, pixiContainer } = this.state;
+
+    if (dragState) {
+      const { originX, originY, mouseX, mouseY, target } = dragState;
+      const { x: relativeParentX, y: relativeParentY } = event.data.getLocalPosition(pixiContainer.parent);
+
+      if (target === names.background) {
+        pixiContainer.x = originX + (relativeParentX - mouseX);
+        pixiContainer.y = originY + (relativeParentY - mouseY);
+      } else if (dragState.target === names.rotateHandle) {
+        const rotateHandle = pixiContainer.getChildByName(names.rotateHandle);
+        rotateHandle.position.y = event.data.getLocalPosition(pixiContainer).y;
+
+        // Pixi's rotation starts at North, but atan2 starts at East, so need to rotate back one quarter.
+        let newRotation = Math.atan2(originY - relativeParentY, originX - relativeParentX) - quarterTurn;
+        newRotation %= fullCircle;
+        newRotation += newRotation < 0 ? fullCircle : 0;
+        pixiContainer.rotation = newRotation;
+      }
+    }
+  }
+
+  onDragEnd = event => {
+    const { stageProp } = this.props;
+    const { dragState, pixiContainer } = this.state;
+    const { x, y } = event.data.getLocalPosition(pixiContainer.parent);
+
+    if (dragState) {
+      if (dragState.target === names.background) {
+        const offsetX = x - dragState.mouseX;
+        const offsetY = y - dragState.mouseY;
+        this.props.updateStageProp({
+          ...stageProp,
+          positionX: Math.round(stageProp.positionX + offsetX),
+          positionY: Math.round(stageProp.positionY + offsetY)
+        });
+      } else if (dragState.target === names.rotateHandle) {
+        const rotation = Math.round(pixiContainer.rotation / Math.PI * 180);
+        this.props.updateStageProp({ ...stageProp, rotation });
+      }
+    }
+
+    this.setState({ dragState: null });
+  }
+
+  getLinePoints(pathProperties, stageProp, segments) {
     const length = pathProperties.getTotalLength();
 
-    const box = { x1: Number.MAX_VALUE, y1: Number.MAX_VALUE, x2: Number.MIN_VALUE, y2: Number.MIN_VALUE };
     const linePoints = [];
-
     const pointCount = Math.floor(length);
     _.forEach(Array(pointCount + 1), (_, i) => {
       const progress = length * (i / pointCount);
       const point = pathProperties.getPointAtLength(progress);
       linePoints.push(point);
-
-      box.x1 = Math.min(box.x1, point.x);
-      box.y1 = Math.min(box.y1, point.y);
-      box.x2 = Math.max(box.x2, point.x);
-      box.y2 = Math.max(box.y2, point.y);
     });
 
-    const width = (box.x2 - box.x1) * stageProp.scaleX;
-    const height = (box.y2 - box.y1) * stageProp.scaleY;
-
-    const points = _.map(linePoints, point => ({
-      x: (point.x - box.x1) * stageProp.scaleX,
-      y: (point.y - box.y1) * stageProp.scaleY
-    }));
-
-    return { points, width, height };
+    return _.map(linePoints, point => ({ x: point.x * stageProp.scaleX, y: point.y * stageProp.scaleY }));
   }
 
   destroyContainer() {
