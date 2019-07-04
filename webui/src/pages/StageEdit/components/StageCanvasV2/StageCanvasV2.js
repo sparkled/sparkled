e6f98f44ecd8c1de93a20113cce4fa5f75b9fc98
Chris Parton <chrisparton1991@gmail.com>
2019-07-05 09:03:32 +1000

    Add background grid to stage editor

--- webui/src/pages/StageEdit/components/StageCanvasV2/StageCanvasV2.js
+++ webui/src/pages/StageEdit/components/StageCanvasV2/StageCanvasV2.js
@@ -6,6 +6,9 @@ import { clamp } from '../../../../utils/numberUtils';
 import { selectStageProp } from '../../actions';
 import StagePropV2 from '../StagePropV2';
 
+const gridSize = 20;
+const zoomLimits = { min: .5, max: 5 };
+
 class StageCanvasV2 extends Component {
 
   state = {
@@ -17,12 +20,16 @@ class StageCanvasV2 extends Component {
     const canvasContainer = document.querySelector(`#${this.state.canvasId}`);
     const parent = canvasContainer.parentElement;
 
-    const pixiApp = new PIXI.Application({ antialias: true, resolution: window.devicePixelRatio || 1 });
+    const resolution = window.devicePixelRatio || 1;
+    const pixiApp = new PIXI.Application({ resolution, antialias: true, transparent: true });
     pixiApp.resizeTo = parent;
     pixiApp.view.style.width = pixiApp.view.style.height = '100%';
+    pixiApp.stage.addChild(this.buildGrid());
     canvasContainer.appendChild(pixiApp.view);
 
     this.setState({ pixiApp }, () => {
+      this.redrawGrid();
+
       pixiApp.renderer.plugins.interaction
         .on('pointerdown', this.onDragStart)
         .on('pointermove', this.onDragMove)
@@ -33,10 +40,20 @@ class StageCanvasV2 extends Component {
     });
   }
 
+  buildGrid = () => {
+    const grid = new PIXI.Graphics();
+    grid.position.x = grid.position.y = 0;
+    grid.name = "Grid";
+    return grid;
+  }
+
   onZoom = event => {
     const { pixiApp } = this.state;
-    const newScale = clamp(pixiApp.stage.scale.x + (event.deltaY / -30), .25, 5);
+
+    const newScale = clamp(pixiApp.stage.scale.x + (event.deltaY / -30), zoomLimits.min, zoomLimits.max);
     pixiApp.stage.scale.x = pixiApp.stage.scale.y = newScale;
+
+    this.redrawGrid();
   }
 
   onDragStart = event => {
@@ -46,9 +63,12 @@ class StageCanvasV2 extends Component {
     }
 
     const { pixiApp } = this.state;
-    const { clientX, clientY } = event.data.originalEvent;
+    const { clientX, clientY, changedTouches } = event.data.originalEvent;
+    let mouseX = clientX !== undefined ? clientX : changedTouches[0].clientX;
+    let mouseY = clientY !== undefined ? clientY : changedTouches[0].clientY;
+
     this.setState({
-      dragState: { originX: pixiApp.stage.x, originY: pixiApp.stage.y, mouseX: clientX, mouseY: clientY }
+      dragState: { originX: pixiApp.stage.x, originY: pixiApp.stage.y, mouseX, mouseY }
     });
   }
 
@@ -57,15 +77,51 @@ class StageCanvasV2 extends Component {
 
     if (dragState) {
       const { originX, originY, mouseX, mouseY } = dragState;
-      const { clientX, clientY } = event.data.originalEvent;
 
-      pixiApp.stage.x = originX + (clientX - mouseX);
-      pixiApp.stage.y = originY + (clientY - mouseY);
+      const { clientX, clientY, changedTouches } = event.data.originalEvent;
+      let newMouseX = clientX !== undefined ? clientX : changedTouches[0].clientX;
+      let newMouseY = clientY !== undefined ? clientY : changedTouches[0].clientY;
+
+      pixiApp.stage.x = originX + (newMouseX - mouseX);
+      pixiApp.stage.y = originY + (newMouseY - mouseY);
+      this.redrawGrid();
     }
   }
 
   onDragEnd = event => {
     this.setState({ dragState: null });
+  }
+
+  redrawGrid = () => {
+    const { pixiApp } = this.state;
+    const grid = pixiApp.stage.getChildByName("Grid");
+    grid.clear();
+
+    const scale = pixiApp.stage.scale.x;
+
+    // Calculate left and top positions for the grid, and subtract gridSize to allow for partial grid cells to be drawn.
+    let startX = (-pixiApp.stage.x / scale) - gridSize;
+    let startY = (-pixiApp.stage.y / scale) - gridSize;
+
+    // Adjust positions to account for the grid offset, otherwise the grid will always be drawn from the top-left.
+    startX -= startX % gridSize;
+    startY -= startY % gridSize;
+
+    // Calculate the width and height, and add gridSize to allow for partial grid cells to be drawn.
+    const width = pixiApp.view.clientWidth / scale + gridSize * 2;
+    const height = pixiApp.view.clientHeight / scale + gridSize * 2;
+
+    grid.lineStyle(1, 0xffffff, .2, 1);
+
+    for (let x = 0; x < width; x += gridSize) {
+      grid.moveTo(x + startX, startY);
+      grid.lineTo(x + startX, height + startY);
+    }
+
+    for (let y = 0; y < height; y += gridSize) {
+      grid.moveTo(startX, y + startY);
+      grid.lineTo(width + startX, y + startY);
+    }
   }
 
   render() {
